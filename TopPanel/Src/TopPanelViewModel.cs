2810b3546794c260af1b052eb3194a6a3695a5bb
ivankadyka <ivankadyko@gmail.com>
2024-09-15 00:07:40 +0200

    add RelayCommand

--- TopPanel/Src/TopPanelViewModel.cs
+++ TopPanel/Src/TopPanelViewModel.cs
@@ -1,6 +1,40 @@
-﻿using PresenterBase.ViewModel;
+﻿using System;
+using System.Windows.Input;
+using PresenterBase.ViewModel;
+using TopPanel.Command;
 
 public class TopPanelViewModel : ViewModelBase
 {
     public string Title { get; set; } = "TopPanel Title";
+    
+    private string _searchAddress;
+    public string SearchAddress
+    {
+        get => _searchAddress;
+        set => SetField(ref _searchAddress, value);
+    }
+
+    // Define the command
+    public ICommand SearchCommand { get; }
+
+    public TopPanelViewModel()
+    {
+        SearchCommand = new RelayCommand(OnSearch);
+    }
+
+    private void OnSearch()
+    {
+        // Implement the action to be taken when Enter is pressed
+        // For example, you could search or process the SearchAddress
+        System.Diagnostics.Debug.WriteLine($"Searching for: {SearchAddress}");
+    }
+    
+    public void OnEnterKeyPressed(object sender, KeyEventArgs e)
+    {
+        if (e.Key == Key.Enter)
+        {
+            Console.WriteLine("Enter" + _searchAddress);
+            SearchCommand.Execute(null);
+        }
+    }
 }
